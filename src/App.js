6ac40b3c0bd5048d64bce76d487eded9e9409d8c
ChaimaeEkh <chaimaeelkhatib645@gmail.com>
2025-02-07 20:15:19 +0100

    chatapp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {ChatEngine} from 'react-chat-engine';
+import './App.css';
+
+export default function App(){
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID=""
+            userName=""
+            userSecret=""
+        />
+    );
+}
